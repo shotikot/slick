6b2407399b7c9119170c42114c676785b4630890
Shotiko Topchishvili <shotikotopchishvili0@gmail.com>
2020-05-31 15:59:26 +0400

    Updates

---
+++ precache-manifest.9813aa07731499d106cee0d5e0d2cdb2.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "300e3a229761d901324b072bd3d77229",
+    "url": "/slick/index.html"
+  },
+  {
+    "revision": "c94b0f3363fa297fe891",
+    "url": "/slick/static/css/main.c6fcdf45.chunk.css"
+  },
+  {
+    "revision": "48005e52e8681eb49c00",
+    "url": "/slick/static/js/2.cc7fd7f6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/slick/static/js/2.cc7fd7f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c94b0f3363fa297fe891",
+    "url": "/slick/static/js/main.d3b440e4.chunk.js"
+  },
+  {
+    "revision": "3a02473f8514b5d82563",
+    "url": "/slick/static/js/runtime-main.5b436628.js"
+  },
+  {
+    "revision": "5bbaa1a4e807b061646481da6c87c4e0",
+    "url": "/slick/static/media/blockchain.5bbaa1a4.svg"
+  },
+  {
+    "revision": "427e30028f0aac00c5748c86effe64a4",
+    "url": "/slick/static/media/bussiness.427e3002.png"
+  },
+  {
+    "revision": "407e750e791c9470be7e9654cad90184",
+    "url": "/slick/static/media/ceo.407e750e.jpg"
+  },
+  {
+    "revision": "fc48d11e2d38b718a077227146837e50",
+    "url": "/slick/static/media/chart.fc48d11e.svg"
+  },
+  {
+    "revision": "66f21ca4774a5211f285f2c36c9039b5",
+    "url": "/slick/static/media/chat.66f21ca4.png"
+  },
+  {
+    "revision": "3bbf3dc564df07b1fb33391a0545652e",
+    "url": "/slick/static/media/heart.3bbf3dc5.svg"
+  },
+  {
+    "revision": "208ec63c8e2a16e00af077c2facb5707",
+    "url": "/slick/static/media/intro.208ec63c.png"
+  },
+  {
+    "revision": "41d267400ca0f91410b96910dd4d3f88",
+    "url": "/slick/static/media/marketer.41d26740.jpg"
+  },
+  {
+    "revision": "e0d65e0065ef3cca52a97b5282293ea2",
+    "url": "/slick/static/media/prog.e0d65e00.jpg"
+  },
+  {
+    "revision": "1061a0304e8cb1909bded252b1f888e2",
+    "url": "/slick/static/media/progger.1061a030.jpg"
+  },
+  {
+    "revision": "c759c3234abf38ec245659eefb2bba06",
+    "url": "/slick/static/media/x.c759c323.svg"
+  }
+]);
